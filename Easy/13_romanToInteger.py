419efe29f745707b7e8fe994df2f6c644a74a096
Yucheng7713 <yuchengh@usc.edu>
2019-08-12 15:08:34 -0700

    08-12-19 Easy_1_Medium_1_Hard_0

--- Easy/13_romanToInteger.py
+++ Easy/13_romanToInteger.py
@@ -6,7 +6,10 @@ class Solution(object):
                   "C": 100,
                   "D": 500,
                   "M": 1000 }
-    def romanToInt(self, s):
+
+    # Starting from the end, so when confronting a subtracting case, there is no need to move the cursor
+    # by two, just simply subtract the previous smaller roman number and keep traversing.
+    def romanToInt_I(self, s):
         L = list(s)
         prevRome = L[len(L) - 1]
         integer = self.romeToInt[prevRome]
@@ -17,6 +20,28 @@ class Solution(object):
                 integer += self.romeToInt[L[i]]
             prevRome = L[i]
         return integer
+
+    # Straight-forwarded solution
+    def romanToInt_II(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        result = 0
+        i = 0
+        while i < len(s):
+            if i != len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                result += roman_map[s[i + 1]] - roman_map[s[i]]
+                i += 2
+            else:
+                result += roman_map[s[i]]
+                i += 1
+        return result
 
 s = Solution()
 romeString = "LVIII"
--- Medium/6_zigZagConversion.py
+++ Medium/6_zigZagConversion.py
@@ -1,5 +1,6 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
+    # Clever way
+    def convert_I(self, s: str, numRows: int) -> str:
         if numRows <= 1 or numRows >= len(s): return s
         zigZag = [""] * numRows
         k, cursor = 0, -1
@@ -9,3 +10,30 @@ class Solution:
             zigZag[k] += c
             k += cursor
         return "".join(zigZag)
+
+    # Classic way to solve the problem
+    # Time complexity : O(S) which S is the length of the string
+    # Space complexity : O(numRows)
+    def convert_II(self, s: str, numRows: int) -> str:
+        if numRows <= 1 or numRows >= len(s):
+            return s
+        z_rows = [""] * numRows
+        up = False
+        i = current_row = 0
+        while i < len(s):
+            if not up:
+                if current_row < numRows:
+                    z_rows[current_row] += s[i]
+                    current_row += 1
+                if current_row > numRows - 1:
+                    current_row -= 2
+                    up = True
+            else:
+                if current_row >= 0:
+                    z_rows[current_row] += s[i]
+                    current_row -= 1
+                if current_row < 0:
+                    current_row += 2
+                    up = False
+            i += 1
+        return "".join(z_rows)
